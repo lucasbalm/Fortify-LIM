a3a299d9a6f7e517b986001b3d07bc4b0557c355
Lucas Almeida <lucas.b.almeida@live.com>
2021-12-27 14:28:06 -0300

    Update start-lim.ps1

--- start-lim.ps1
+++ start-lim.ps1
@@ -1,1 +1,1 @@
-docker run -v c:/tools/lim:c:/lim -ti -p 8090:80 --restart always --env-file .\LimDocker.env --memory=8g --cpus=2 --name lim fortifydocker/lim:21.2
+docker run -v c:/tools/lim:c:/lim -ti -p 8090:80 -d --restart always --env-file .\LimDocker.env --memory=8g --cpus=2 --name lim fortifydocker/lim:21.2
